91b9ca594bda2fff2042edef689ebd79da82452a
Mohammed Qaid <eng.mo.amri@gmail.com>
2024-03-17 11:05:58 +0300

    Fix the relationship implementation between entities

--- src/main/java/com/itep/restaurant_service/repositories/entities/DayEntity.java
+++ src/main/java/com/itep/restaurant_service/repositories/entities/DayEntity.java
@@ -1,8 +1,17 @@
 package com.itep.restaurant_service.repositories.entities;
 
 
+import com.itep.restaurant_service.models.DayResource;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "days")
 public class DayEntity {
@@ -11,12 +20,16 @@ public class DayEntity {
     private long id;
     @Column(name = "name")
     private String name;
-
-    DayEntity() {
-    }
-
-    public DayEntity(String name) {
-        this.name = name;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "shift_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private ShiftEntity shift;
+    public DayResource toDayResource() {
+        return DayResource
+                .builder()
+                .id(id)
+                .name(name)
+                .build();
     }
 
 }
