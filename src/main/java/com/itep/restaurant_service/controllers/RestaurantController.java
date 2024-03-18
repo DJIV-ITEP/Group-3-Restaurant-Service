dcb4c5565077b5880d746aafb2910219f047b4e5
Mohammed Qaid <eng.mo.amri@gmail.com>
2024-03-18 11:11:42 +0300

    implement admin authentication to create restaurants

--- src/main/java/com/itep/restaurant_service/controllers/RestaurantController.java
+++ src/main/java/com/itep/restaurant_service/controllers/RestaurantController.java
@@ -5,11 +5,15 @@ import com.itep.restaurant_service.repositories.entities.RestaurantEntity;
 import com.itep.restaurant_service.services.impl.RestaurantServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -26,6 +30,7 @@ public class RestaurantController {
         return restaurantService.getAvailableRestaurants();
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/restaurants")
     public ResponseEntity<Object> createRestaurant(@RequestBody RestaurantEntity body)  {
         try {
