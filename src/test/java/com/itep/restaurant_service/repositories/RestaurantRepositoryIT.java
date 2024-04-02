24e74ddb09b84d57919d6c320966f43ee5b21d1e
Mohammed Qaid <eng.mo.amri@gmail.com>
2024-04-02 11:48:26 +0300

    start implementing integration test for restaurant repository

--- src/test/java/com/itep/restaurant_service/repositories/RestaurantRepositoryIT.java
+++ src/test/java/com/itep/restaurant_service/repositories/RestaurantRepositoryIT.java
@@ -1,18 +1,17 @@
 package com.itep.restaurant_service.repositories;
 
+import com.itep.restaurant_service.repositories.entities.CategoryEntity;
 import com.itep.restaurant_service.repositories.entities.RestaurantEntity;
 import com.itep.restaurant_service.repositories.entities.UserEntity;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,16 +21,52 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RestaurantRepositoryIT {
     @Autowired
     private RestaurantRepository restaurantRepository;
-    @BeforeEach
-    public void setup() throws Exception {
-        RestaurantEntity restaurant = new RestaurantEntity(1, "name", "address", "location", "online", "food", "cuisine", new UserEntity("owner", "owner"), new ArrayList<>());
-        restaurantRepository.save(restaurant);
-    }
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private MenuRepository menuRepository;
+    @Autowired
+    private MenuItemRepository menuItemRepository;
+
     @Test
+    @Transactional
     void setRestaurantStatus(){
-        restaurantRepository.updateStatus(1,"online");
+        restaurantRepository.save(new RestaurantEntity(1L, "R1", "address", "location", "offline", "seafood", "cuisine",
+                new UserEntity("o1", "o1"),
+                new ArrayList<>()));
+        restaurantRepository.updateStatus(1L,"online");
         var restaurant = restaurantRepository.findById(1L);
+        System.out.println(restaurant);
         assertThat(restaurant).isPresent();
         assertThat(restaurant.get().getStatus()).isEqualTo("online");
     }
+    @Test
+    void getAvailableRestaurantsAfterSettingRestaurantStatus(){
+        var r1 = new RestaurantEntity(1, "R1", "address", "location", "offline", "seafood", "cuisine",
+                new UserEntity("o1", "o1"),
+                new ArrayList<>());
+        restaurantRepository.save(r1);
+        var r2 = new RestaurantEntity(2, "R2", "address", "location", "offline", "vegan", "cuisine",
+                new UserEntity("o2", "o2"),
+                new ArrayList<>());
+        restaurantRepository.save(r2);
+        restaurantRepository.updateStatus(r2.getId(),"online");
+        var availableRestaurants = restaurantRepository.findByStatus("online");
+        assertThat(availableRestaurants).hasSize(1);
+        assertThat(availableRestaurants.get(0).getId()).isEqualTo(r2.getId());
+    }
+    @Test
+    void getRestaurantCategoriesAfterAddingOneCategory(){
+        var r1 = new RestaurantEntity(1, "R1", "address", "location", "offline", "seafood", "cuisine",
+                new UserEntity("o1", "o1"),
+                new ArrayList<>());
+        var c1 = new CategoryEntity(1, "c1", r1, new ArrayList<>());
+        restaurantRepository.save(r1);
+        categoryRepository.save(c1);
+        var restaurantCategories = categoryRepository.findByRestaurantId(1L);
+        assertThat(restaurantCategories).hasSize(1);
+        assertThat(restaurantCategories.get(0).getRestaurant().getId()).isEqualTo(c1.getRestaurant().getId());
+        assertThat(restaurantCategories.get(0).getId()).isEqualTo(c1.getId());
+    }
 }
+
