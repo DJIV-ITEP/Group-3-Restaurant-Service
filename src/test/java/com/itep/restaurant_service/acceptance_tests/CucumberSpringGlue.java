446ba60a4ac3486b5ee4fa94a7a7cab4e5999156
azzadeen2 <ameer.azzadeen@gmail.com>
2024-04-13 03:39:21 +0300

    stop all test

--- src/test/java/com/itep/restaurant_service/acceptance_tests/CucumberSpringGlue.java
+++ src/test/java/com/itep/restaurant_service/acceptance_tests/CucumberSpringGlue.java
@@ -1,14 +1,14 @@
-//package com.itep.restaurant_service.acceptance_tests;
-//import io.cucumber.spring.CucumberContextConfiguration;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//@CucumberContextConfiguration
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-//@Import({WireMockServerConfiguration.class})
-//@AutoConfigureMockMvc
-//@ActiveProfiles("dev")
-//public class CucumberSpringGlue {
-//}
+package com.itep.restaurant_service.acceptance_tests;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Import({WireMockServerConfiguration.class})
+@AutoConfigureMockMvc
+@ActiveProfiles("dev")
+public class CucumberSpringGlue {
+}
